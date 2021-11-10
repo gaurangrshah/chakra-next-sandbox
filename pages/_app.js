60cb0e35e91f138d1a65b187a776b19d0c924f42
gshah2020 <gaurang.r.shah@gmail.com>
2021-11-10 13:54:25 -0500

    01.getting-started-sandbox

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../chakra";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  console.log({ theme });
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
